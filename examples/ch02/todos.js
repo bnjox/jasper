33352e0586d55fba6070069e1b2a3308af4b4362
Sparrow <benjaminthorpe19@gmail.com>
2025-10-23 13:13:42 +0000

    a basic skeleton todo app

--- examples/ch02/todos.js
+++ examples/ch02/todos.js
@@ -1,1 +1,36 @@
-//
+// State of the app
+const todo = ['Walk the dog', 'Water the plants', 'Sand the chairs']
+
+// HTML element references
+const add_todo_input = document.getElementById('todo-input')
+const add_todo_btn = document.getElementById('add-todo-btn')
+const todos_list = document.getElementById('todos-list')
+
+
+// Initialize the view
+for (const todo of todo) {
+  todos_list.append(render_todo_in_read_mode(todo))
+}
+
+add_todo_input.addEventListener('input', () => {
+  add_todo_btn.disabled = add_todo_input.value.length < 3
+})
+
+add_todo_input.addEventListener('keydown', ({ key }) => {
+  if (key === 'Enter' && add_todo_input.value.length >= 3) {
+    add_todo()
+  }
+})
+
+add_todo_btn.addEventListener('click', () => {
+  add_todo()
+})
+
+// Functions
+function render_todo_in_read_mode(_todo) {
+  // TODO: implement me!
+}
+
+function add_todo() {
+  // TODO: implement me!
+}
